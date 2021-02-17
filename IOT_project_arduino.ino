c093e560f4b170dcc92b569199678d8a77edb9ef
Ketaki Keluskar <47587003+Kjkeluskar@users.noreply.github.com>
2021-02-17 15:42:02 +0530

    Update IOT_project_arduino.ino

--- IOT_project_arduino.ino
+++ IOT_project_arduino.ino
@@ -8,69 +8,103 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define echoPin 12 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 13 //attach pin D3 Arduino to pin Trig of HC-SR04
+int myCounter = 0; //declare your variable myCounter and set to 0
 
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
 SoftwareSerial Serial1(10, 11);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); //Declaring the display name (display)
+Adafruit_SSD1306 display1(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); //Declaring the display name (display)
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-String str;
+String str, str1;
+int temp=0;
 int ambTemp;
 int objTemp;
 
 void setup() {
-  Serial.begin(115200);
-  Serial1.begin(115200);  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
   mlx.begin(); 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //Start the OLED display
   display.clearDisplay();
   display.display();
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
+  display.setTextSize(1);                    
+  display.setTextColor(WHITE);             
+  display.setCursor(0,4);                
+  display.println("Distance:");
+  
 
 }
 
 void loop() {
-  display.clearDisplay();
-  digitalWrite(9, HIGH);
-  delay(3000); //delay 4 secs
-  digitalWrite(9, HIGH);
-  digitalWrite(9, HIGH);
-  display.setTextSize(1);                    
+  digitalWrite(trigPin, LOW); //Set trigger pin low
+  delayMicroseconds(2000); //Let signal settle
+  digitalWrite(trigPin, HIGH); //Set trigPin high
+  delayMicroseconds(15); //Delay in high state
+  digitalWrite(trigPin, LOW); //ping has now been sent
+  delayMicroseconds(10); //Delay in high state
+
+   digitalWrite(trigPin, LOW);
+  
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  digitalWrite(10, HIGH);
+
+  if (distance > 20) {
+      display.setTextSize(1);
+      display.setCursor(40,2);
+      display.println("Come Closer");
+      digitalWrite(10, HIGH);
+      display.display();
+      display.clearDisplay();
+    
+  }
+  else if (distance < 20) {
+      display.setTextSize(1);
+  display.setCursor(40,2);
+  display.println("Hold steady");
+  
+    digitalWrite(10, HIGH);
+    delay(2000);
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
+    display.setTextSize(1);                    
   display.setTextColor(WHITE);             
-  display.setCursor(0,4);                
-  display.println("Ambient"); 
-  
-  display.setTextSize(2);
-  display.setCursor(50,0);
-  display.println(mlx.readAmbientTempC(),1);
- 
-  display.setCursor(110,0);
-  display.println("C");
-  
-  display.setTextSize(1);                    
-  display.setTextColor(WHITE);             
-  display.setCursor(0,20);                
-  display.println("Target"); 
-  
-  display.setTextSize(2);
-  display.setCursor(50,17);
-  display.println(mlx.readObjectTempC(),1);
-  
-  display.setCursor(110,17);
-  display.println("C");
+  display.setCursor(0,22);                
+  display.println("Target");
+  display.display();
+  delay(2000);
+    display.setTextSize(2);
+    display.setCursor(50,17);
+    display.println(mlx.readObjectTempC(),1);
+    
+    display.setCursor(110,17);
+    display.println("C");
+    if(mlx.readObjectTempC()>32)
+    {
+      //INPUT - FREQUENCY - TIME THAT LASTS
+    tone(8, 800, 300);
+    }
   
   display.display();
-
-  str =String('A')+String(mlx.readAmbientTempC())+String(' ') + String('O')+String(mlx.readObjectTempC());
-  Serial1.println(str);
-
-  if(mlx.readObjectTempC()>35) {
-    //INPUT - FREQUENCY - TIME THAT LASTS
-    tone(8, 800, 300);
   }
+  display.clearDisplay();
+  delay(2000);
   digitalWrite(9, LOW);
-  
   digitalWrite(10, HIGH);
-  delay(10000); //delay 10 secs
-  digitalWrite(10, LOW);
+  
+
 }
